978fd22c48a45c57c41e07786c7950a0bf023829
IMRANDIL <aliimranadil2@gmail.com>
2022-01-07 20:05:19 +0530

    YH

--- routes/people.js
+++ routes/people.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { append } = require('express/lib/response');
 const connection = require('../connection');
 
 //create a new record...
@@ -12,9 +13,52 @@ router.post('/create', (req, res) => {
     });
 
 
+});
+
+//Read all Records...
+
+router.get('/show', (req, res) => {
+    const sql = `SELECT  * FROM people`;
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json({ result: results })
+    })
+});
+
+//show Specific Record...
+
+router.get('/show/:id', (req, res) => {
+    const _id = req.params.id;
+    const sql = `SELECT * FROM people WHERE id=${_id}`;
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json({ data: result })
+    })
 })
 
+//update a Specific record..
 
+router.put('/show/:id', (req, res) => {
+    const _id = req.params.id;
+    const sql = `UPDATE people SET name='${req.body.name}',location='${req.body.location}' WHERE id='${_id}'`;
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json({ msg: 'successfully edited' })
+    })
+});
+
+
+//delete a Specific record....
+
+
+router.delete('/show/:id', (req, res) => {
+    const _id = req.params.id;
+    const sql = `DELETE FROM people WHERE id=${_id}`;
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json({ msg: `successfully deleted id ${_id}` })
+    })
+})
 
 
 
